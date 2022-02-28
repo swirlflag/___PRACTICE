f54a442e0cf8ad2ffdd69acfa5e1038d602a0e2e
swirlflag <swirlflag@gmail.com>
2022-02-28 10:59:59 +0900

    three yt3 start

---
+++ 2022/three/02.begin-yt/3/index.js
@@ -0,0 +1,8 @@
+
+// Find the latest version by visiting https://cdn.skypack.dev/three.
+
+import * as THREE from 'https://cdn.skypack.dev/three@0.137.5';
+
+const scene = new THREE.Scene();
+
+console.log(scene);
