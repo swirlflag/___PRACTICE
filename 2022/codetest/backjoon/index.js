30d8452a9627af6e2e961b32d09922210eab5d32
swirlflag <swirlflag@gmail.com>
2022-05-02 23:26:15 +0900

    sort

--- 2022/codetest/backjoon/index.js
+++ 2022/codetest/backjoon/index.js
@@ -1,26 +1,52 @@
-// const inputs = fs.readFileSync('/dev/stdin', 'utf8').toString().trim().split('\n');
+//get inputs
+const fs = require("fs");
+const inputs = fs
+	.readFileSync("./dev/stdin", "utf8")
+	.toString()
+	.trim()
+	.split(" ");
+const inputNumber = Math.abs(inputs[0]);
 
-const fs = require('fs');
-const inputs = fs.readFileSync('./dev/stdin', 'utf8').toString().trim().split('\n');
-const inputArray = inputs.map((c) => +c);
+//code
+const solution = (number) => {
 
-let solution = (numbers) => {
-    let result = 0;
-    const divNumber = 42;
-
-    let countArray = [];
-    for(let i = 0; i < numbers.length; ++i) {
-        const number = numbers[i]%divNumber;
-        if(!countArray.includes(number)) {
-            countArray.push(number);
-        }
+    const isCenterCheck = (ratio) => {
+        return ratio >= 1/3 && ratio < 2/3;
     }
 
-    result = countArray.length;
 
-    return result;
-}
+    const loop = (count = 1) => {
 
-const result = solution(inputArray);
+        const map = [];
+
+        for(let x = 0; x < number; ++x) {
+            const line = [];
+            for(let y = 0; y < number; ++y) {
+                const isCenterRow = isCenterCheck(x/(number-1)) ;
+                const isCenterCol = isCenterCheck(y/(number-1)) ;
+                const isCenter = isCenterRow && isCenterCol;
+                const unit = count === number/3 ? [loop(count + 1) , []] : ['*', '-'];
+                line.push(isCenter ? unit[1]: unit[0]);
+            }
+            map.push(line);
+        }
+
+        return map;
+    }
+
+    const map = loop();
+
+    // console.log();
+    console.log(map);
+
+    // map.forEach((line) => {
+    //     console.log(line.join(''));
+    // })
+
+
+};
+
+//result
+const result = solution(inputNumber);
 
 console.log(result);
