3853f72b9b871cddda1cff29f9c6922d080ae605
swirlflag <swirlflag@gmail.com>
2022-06-26 01:29:03 +0900

    front error

--- 2022/nodebird/prepare/front/components/imagesZoom/index.jsx
+++ 2022/nodebird/prepare/front/components/imagesZoom/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Slick from "react-slick";
-import { backUrl } from "../config/config.js";
+import { backUrl } from "../../config/config.js";
 
 import {
     Overlay,
--- 2022/nodebird/prepare/front/config/config.js
+++ 2022/nodebird/prepare/front/config/config.js
@@ -1,3 +1,3 @@
 const isProd = process.env.NODE_ENV === "production";
 
-export const backUrl = isProd ? "http://15.164.50.126" : "http://localhost:3065";
+export const backUrl = isProd ? "http://15.164.50.126" : "http://localhost:80";
