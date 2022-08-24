e23a1c1f4bc1543754a5538770f5e2cc0e87716d
swirlflag <swirlflag@gmail.com>
2022-08-24 19:07:31 +0900

    cheatcheet

--- 2022/gsap-cheetsheet /script.js
+++ 2022/gsap-cheetsheet /script.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Flip);
 
 const scoped = (fn) => fn();
 const sayTitle = (area, title) => {
@@ -6,6 +6,14 @@ const sayTitle = (area, title) => {
     p.innerText = title;
     area.insertBefore(p, area.firstChild);
 };
+
+ScrollTrigger.defaults({
+    start: "0% 70%",
+    end: "100% 30%",
+    markers: true,
+})
+
+const move = document.querySelector(".area-0 .move");
 
 // basic
 scoped(() => {
@@ -23,8 +31,7 @@ scoped(() => {
 		scrollTrigger: {
 			trigger: area,
 			// endTrigger: area,
-			start: "0% 90%",
-			end: "100% 0%",
+
 			scrub: 1,
 		},
 	});
@@ -37,8 +44,7 @@ scoped(() => {
 			duration: 1,
 		}),
 		trigger: area,
-		start: "0 90%",
-		end: "100% 0%",
+
 		scrub: 1,
 	});
 });
@@ -68,8 +74,6 @@ scoped(() => {
     area_st = ScrollTrigger.create({
         animation: tl,
         trigger: area,
-        start: "0% 80%",
-        end: "100% 0%",
         scrub: 1,
     });
 
@@ -84,9 +88,6 @@ scoped(() => {
         }),
         trigger: area,
         scrub: 1,
-        start: "0% 80%",
-        end: "100% 0%",
-        makers: true,
 	});
 
 });
@@ -119,10 +120,7 @@ scoped(() => {
         st = ScrollTrigger.create({
             animation: tl,
             trigger: area,
-            start: "0% 80%",
-            end: "100% 0%",
             scrub: 1,
-            makers: true,
         });
 
     };
@@ -137,21 +135,87 @@ scoped(() => {
 scoped(() => {
     const area = document.querySelector(".area-4");
     sayTitle(area, "timing methods");
-	// onUpdate: self => console.log("progress", self.progress)
-	// onProgress:
-	// onEnter:
-	// onEnterBack
-	// onLeave
-	// onLeaveBack
-	// onScrubComplete
-	// onSnapComplete
-	// onToggle
+
+    const move = area.querySelector(".move");
+    const log = area.querySelector(".log");
+    const logSlots = log.querySelectorAll("p > span");
+
+    const alertAnimation = [
+        {
+            color: "#f00",
+            scale: 1.5,
+        } ,
+        {
+            color : "#000" ,
+            scale: 1,
+            ease: "power2.inOut",
+            duration: 0.6,
+        }
+    ]
+
+    ScrollTrigger.create({
+        animation: gsap.to(move, {x: area.offsetWidth - move.offsetWidth}),
+        trigger: area,
+        scrub: 1,
+        onUpdate(v) {
+            logSlots[0].innerText = `progress : ${v.progress}`;
+        },
+        onEnter(v) {
+            gsap.fromTo(logSlots[1] , ...alertAnimation);
+        },
+        onEnterBack(v) {
+            gsap.fromTo(logSlots[2] , ...alertAnimation);
+        },
+        onLeave() {
+            gsap.fromTo(logSlots[3] , ...alertAnimation);
+        },
+        onLeaveBack() {
+            gsap.fromTo(logSlots[4] , ...alertAnimation);
+        },
+        onScrubComplete(progress) {
+            gsap.fromTo(logSlots[5] , ...alertAnimation);
+        },
+        onSnapComplete() {
+        },
+        onToggle(v) {
+            gsap.fromTo(logSlots[6] , ...alertAnimation);
+        }
+    });
 });
 
 // pin
 scoped(() => {
     const area = document.querySelector(".area-5");
     sayTitle(area, "pin");
+    const move = area.querySelectorAll(".move");
+    gsap.set(area , {height: "100vh"})
+    gsap.set(move, {position: "absolute", top: 0, left: 0});
+
+    const tl = gsap.timeline();
+    [...move].map((item,i) => {
+        const ease = i%2 ? ["power1.out", "power1.in"] : ["power1.in", "power1.out"];
+        const timing = i * 0.02 + (i%2 ? 0.01 : 0);
+
+        tl.to(item, {
+            x: area.offsetWidth - item.offsetWidth,
+            ease : ease[0],
+        }, timing);
+        tl.to(item, {
+            y: area.offsetHeight - item.offsetHeight,
+            ease : ease[1],
+        }, timing);
+    });
+
+    ScrollTrigger.create({
+        animation: tl,
+        trigger: area,
+        start: "0% 0%",
+        end: "100% 0%",
+        pin: true,
+        ease: "power2.out",
+        scrub: 0.2,
+        pinSpacing: true,
+    });
 });
 
 // match media
@@ -160,4 +224,44 @@ scoped(() => {
     sayTitle(area, "match media");
 });
 
+// flip + InertiaPlugin
+scoped(() => {
+    const area = document.querySelector(".area-0");
+    sayTitle(area, "flip + InertiaPlugin");
+    const move = area.querySelector(".move");
 
+    const state = Flip.getState(move);
+
+    // move.style.position = "fixed";
+    // move.style.top = 0;
+    // move.style.left = 0;
+
+    move.style.left = '100px';
+
+
+
+    const myFunc = () => {
+        console.log('d');
+    }
+
+
+    ScrollTrigger.create({
+        trigger: area,
+        onEnter: () => {
+
+        }
+    })
+    Flip.from(state, {
+        duration: 1,
+        ease: "power1.inOut",
+        // absolute: true,
+        fixed: true,
+        onComplete: myFunc
+    });
+});
+
+// quickTO
+
+// Physics2D
+
+// horizontal
